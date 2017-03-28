341894aad448b000611375540127f116e4137de4
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2017-03-28 09:45:00 +0300

    Added header footer and content shells

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'be';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+$route['default_controller'] = 'home';
+
+// Front End
+
+
 
 
 
@@ -57,5 +63,4 @@ $route['default_controller'] = 'be';
 $route['be'] = 'be/main';
 
 
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+
