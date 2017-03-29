ec34dc42d339ebd11dea6a118325be4c08de35fb
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2017-03-29 18:38:35 +0300

    Cleaned up header

--- application/controllers/be/Auth.php
+++ application/controllers/be/Auth.php
@@ -1,16 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
-	
+
 	function __construct(){
 		parent::__construct();
 		$this->load->model('be/login_model');
 	}
-	
+
 	function index(){
 		if($this->session->userdata('nhub_loginstate')){
             redirect('be');
-        } 
+        }
 		else{
 			if ($this->login_model->check_super_admin() == false){
 				$this->load->view('be/register');
@@ -28,7 +28,7 @@ class Auth extends CI_Controller {
 				'email_address' => $this->input->post('email_address'),
 				'user_password' => $user_password,
 				'is_super_admin' => 1
-			);	
+			);
 			$q = $this->login_model->register_user($data);
 			if($q['res'] == true){
 				$this->session->set_flashdata('success', 'Registration successful. Please login to continue');
@@ -45,28 +45,28 @@ class Auth extends CI_Controller {
 		}
 
 	}
-	function login(){		
+	function login(){
 		$query = $this->login_model->validate();
-		
-		if($query['res'] == true){			
+
+		if($query['res'] == true){
 			$this->session->set_userdata('nhub_loginstate', TRUE);
 			$this->session->set_userdata('user_id', $query['user_id']);
 			$this->session->set_userdata('user_email', $this->input->post('email_address'));
-						
+
 			redirect('be');
-		}		
+		}
 		else{
 			$data['incorrect'] = 'Invalid credentials';
 			$data['nhub_loginstate'] = FALSE;
-			$this->load->view('be/login',$data);		
+			$this->load->view('be/login',$data);
 		}
 	}
-	
+
 	function logout(){
 		$this->session->sess_destroy();
 		$this->index();
 	}
-	
+
 	function pswupdate(){
 		$this->load->model('be/login_model');
 		$this->login_model->pswupdate();
